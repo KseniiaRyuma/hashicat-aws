506f930bd38582c8ec72a9a1fca1bacc1578b021
Kseniia Ryuma <kseniya0213@gmail.com>
2020-03-05 11:51:00 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
-  default     = "ryuma"
+  default     = "kseniiaryuma"
 }
 
 variable "region" {
